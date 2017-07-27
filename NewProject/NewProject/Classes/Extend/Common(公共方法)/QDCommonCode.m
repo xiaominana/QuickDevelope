c4d82817b64d1c055a06a8302fca7b6b13ba5b67
董晓伟 <1129835747@qq.com>
2017-07-27 16:44:21 +0800

    添加 QDCommonCode - UILabel

--- NewProject/NewProject/Classes/Extend/Common(公共方法)/QDCommonCode.m
+++ NewProject/NewProject/Classes/Extend/Common(公共方法)/QDCommonCode.m
@@ -10,10 +10,71 @@
 
 @implementation QDCommonCode
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 UILabel * label;
 
-#pragma mark - 1 UILabel
-#pragma mark - 设置UILabel行间距
+#pragma mark - UILabel
+
+#pragma mark - UILabel常用属性
++(UILabel *)createLabelWithFrame:(CGRect)frame Text:(NSString *)text font:(UIFont *)font textColor:(UIColor *)textColor
+{
+    UILabel * label = [[UILabel alloc]initWithFrame:frame];
+    label.numberOfLines = 0;
+    label.text = text;
+    label.font = font;
+    label.textColor = textColor;
+    [label sizeToFit];
+    return label;
+}
+
+#pragma mark - UILabel在指定的地方换行
+-(void)funcUILabelNextLine
+{
+    // 换行符为\n,在需要换行的地方加上这个符号即可，如
+    label.numberOfLines = 0;
+    label.text = @"此处\n换行";
+}
+
+#pragma mark - UILabel在指定的宽度下，自动设置最佳font
+-(void)funcUILabelSetFont
+{
+    label.adjustsFontSizeToFitWidth = YES;
+}
+
+#pragma mark - UILabel在指定字体下，自动设置 size
+-(void)funcUILabelSizeToFit
+{
+    [label sizeToFit];
+}
+
+#pragma mark - UILabel设置行间距
 -(void)funcUILabelLineSpace
 {
     NSMutableAttributedString* attrString = [[NSMutableAttributedString  alloc] initWithString:label.text];
@@ -34,7 +95,101 @@ UILabel * label;
     label.attributedText = string;
 }
 
+#pragma mark - UILabel计算label上某段文字的frame
+- (CGRect)boundingRectForCharacterRange:(NSRange)range
+{
+    NSTextStorage *textStorage = [[NSTextStorage alloc] initWithAttributedString:[label attributedText]];
+    NSLayoutManager *layoutManager = [[NSLayoutManager alloc] init];
+    [textStorage addLayoutManager:layoutManager];
+    NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize:[label bounds].size];
+    textContainer.lineFragmentPadding = 0;
+    [layoutManager addTextContainer:textContainer];
+    NSRange glyphRange;
+    [layoutManager characterRangeForGlyphRange:range actualGlyphRange:&glyphRange];
+    return [layoutManager boundingRectForGlyphRange:glyphRange inTextContainer:textContainer];
+}
 
+#pragma mark - UILabel的文字内容显示在左上／右上／左下／右下／中心顶／中心底部
+//自定义UILabel
+//重写label的textRectForBounds方法
+/*
+- (CGRect)textRectForBounds:(CGRect)bounds limitedToNumberOfLines:(NSInteger)numberOfLines {
+    CGRect rect = [super textRectForBounds:bounds limitedToNumberOfLines:numberOfLines];
+    switch (self.textAlignmentType) {
+        case WZBTextAlignmentTypeLeftTop: {
+            rect.origin = bounds.origin;
+        }
+            break;
+        case WZBTextAlignmentTypeRightTop: {
+            rect.origin = CGPointMake(CGRectGetMaxX(bounds) - rect.size.width, bounds.origin.y);
+        }
+            break;
+        case WZBTextAlignmentTypeLeftBottom: {
+            rect.origin = CGPointMake(bounds.origin.x, CGRectGetMaxY(bounds) - rect.size.height);
+        }
+            break;
+        case WZBTextAlignmentTypeRightBottom: {
+            rect.origin = CGPointMake(CGRectGetMaxX(bounds) - rect.size.width, CGRectGetMaxY(bounds) - rect.size.height);
+        }
+            break;
+        case WZBTextAlignmentTypeTopCenter: {
+            rect.origin = CGPointMake((CGRectGetWidth(bounds) - CGRectGetWidth(rect)) / 2, CGRectGetMaxY(bounds) - rect.origin.y);
+        }
+            break;
+        case WZBTextAlignmentTypeBottomCenter: {
+            rect.origin = CGPointMake((CGRectGetWidth(bounds) - CGRectGetWidth(rect)) / 2, CGRectGetMaxY(bounds) - CGRectGetMaxY(bounds) - rect.size.height);
+        }
+            break;
+        case WZBTextAlignmentTypeLeft: {
+            rect.origin = CGPointMake(0, rect.origin.y);
+        }
+            break;
+        case WZBTextAlignmentTypeRight: {
+            rect.origin = CGPointMake(rect.origin.x, 0);
+        }
+            break;
+        case WZBTextAlignmentTypeCenter: {
+            rect.origin = CGPointMake((CGRectGetWidth(bounds) - CGRectGetWidth(rect)) / 2, (CGRectGetHeight(bounds) - CGRectGetHeight(rect)) / 2);
+        }
+            break;
+            
+        default:
+            break;
+    }
+    return rect;
+}
+ */
+
+#pragma mark - UILabel设置内边距
+//子类化UILabel，重写drawTextInRect方法
+
+/*
+- (void)drawTextInRect:(CGRect)rect {
+    // 边距，上左下右
+    UIEdgeInsets insets = {0, 5, 0, 5};
+    [super drawTextInRect:UIEdgeInsetsInsetRect(rect, insets)];
+}
+ */
+
+#pragma mark - UILabel设置文字描边
+//子类化UILabel，重写drawTextInRect方法
+/*
+- (void)drawTextInRect:(CGRect)rect
+{
+    CGContextRef c = UIGraphicsGetCurrentContext();
+    // 设置描边宽度
+    CGContextSetLineWidth(c, 1);
+    CGContextSetLineJoin(c, kCGLineJoinRound);
+    CGContextSetTextDrawingMode(c, kCGTextStroke);
+    // 描边颜色
+    self.textColor = [UIColor redColor];
+    [super drawTextInRect:rect];
+    // 文本颜色
+    self.textColor = [UIColor yellowColor];
+    CGContextSetTextDrawingMode(c, kCGTextFill);
+    [super drawTextInRect:rect];
+}
+ */
 
 
 
